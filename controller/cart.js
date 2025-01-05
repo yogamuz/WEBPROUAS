e140dd6f1c838508337fb9865c1c3e8ca1d22751
yogamuz <yogamuz13@gmail.com>
2025-01-05 18:00:49 +0700

    update cart bug pt2

--- controller/cart.js
+++ controller/cart.js
@@ -2,7 +2,7 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
 
 if (!cart) {
     cart = [{
-        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        productId: '',
         quantity: 0,
     }]
 
